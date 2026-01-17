ca9c86d14f8dab7b4c11d9b375e198fb9fba7d72
Miroslav <miroslavberezin93@gmail.com>
2026-01-18 01:31:23 +0700

    Add client refresh interceptor

--- src/api/authApi.ts
+++ src/api/authApi.ts
@@ -1,10 +1,16 @@
 import { api } from "./client";
 import type { RegisterDto } from "../dto/registerDto";
 import type { TokenResponseDto } from "../dto/tokenReponseDto";
+import type { LoginDto } from "../dto/loginDto";
 
 export const authApi = {
     async registerAsync(dto: RegisterDto): Promise<TokenResponseDto>{
         const res = await api.post<TokenResponseDto>("api/auth/register",dto);
+        return res.data;
+    },
+    
+    async loginAsync(dto: LoginDto ): Promise<TokenResponseDto>{
+        const res = await api.post<TokenResponseDto>("api/auth/login", dto);
         return res.data;
     }
 }
--- src/api/client.ts
+++ src/api/client.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { authStore } from "../store/authStore";
+import type { TokenResponseDto } from "../dto/tokenReponseDto";
 
 export const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
@@ -13,4 +14,32 @@ api.interceptors.request.use((config) => {
     const token = authStore.getState().accessToken;
     if (token) config.headers.Authorization = `Bearer ${token}`;
     return config;
-})
+});
+
+api.interceptors.response.use(
+    (res) => res,
+    async (error) => {
+        const originalReq = error.config;
+        
+        if(error.response?.status === 401 && !originalReq._retry) {
+            originalReq._retry = true;
+
+            try {
+                const res = await api.post<TokenResponseDto>("api/auth/refresh");
+                authStore.getState().setAccessToken(res.data.accessToken);
+                originalReq.headers.Authorization = `Bearer ${res.data.accessToken}`;
+                return api(originalReq);
+            } catch {
+                logout();
+                return Promise.reject(error);
+            }
+        }
+
+        return Promise.reject(error);
+    }
+);
+
+function logout(): void {
+    authStore.getState().setAccessToken(null);
+    authStore.getState().setIsAuth(false);
+}
